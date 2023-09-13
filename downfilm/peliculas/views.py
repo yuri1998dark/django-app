24fe0545c003f265e73575f3f6384ee20bfebb07
yuri1998dark <ubu@localhost.localdomain>
2023-09-13 07:49:51 -0400

    requerimientos agregados

--- downfilm/peliculas/views.py
+++ downfilm/peliculas/views.py
@@ -10,15 +10,16 @@ from bs4 import BeautifulSoup
 
 def busqueda_view(request):
     template_name = 'busqueda.html'
+    
     if request.method == 'POST':
         form = BusquedaForm(request.POST)
         if form.is_valid():
             pelicula = form.cleaned_data['pelicula']
-            response = requests.get(f'http://www.omdbapi.com/?apikey=93414632&t={pelicula}')
+            response = requests.get(f'http://www.omdbapi.com/?apikey=93414632&s={pelicula}&page=2')
             print(response)
             if response.status_code == 200:
                 datos_json = response.json()
-                # print(datos_json)
+                print(datos_json)
                 
                 return render(request, template_name, {'form': form, 'query': datos_json})
             
@@ -50,7 +51,7 @@ def descargar_pelicula_view(request,year,titulo:str):
                 # print(link)
                 href = link.get('href')
                 # print(href)
-                if href and href.endswith(('.mp4', '.mkv', '.mpg')):
+                if href and href.endswith(('.mp4', '.mkv', '.mpg','srt')):
                     videos.append(href)
                     print(videos)
                    
